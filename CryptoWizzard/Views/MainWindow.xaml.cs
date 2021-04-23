0467c27570dd0a9a2f4f8db6727a3eaa1987b13a
asceso <asceso111@gmail.com>
2021-04-23 08:21:33 +0300

    Добавьте файлы проекта.

---
+++ CryptoWizzard/Views/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace CryptoWizzard.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            Width = SystemParameters.FullPrimaryScreenWidth - 10;
+            Height = SystemParameters.FullPrimaryScreenHeight + 10;
+            InitializeComponent();
+        }
+    }
+}
